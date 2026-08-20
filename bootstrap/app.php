4bb389bd7a1c352ef08fa22b61cba84302419130
John Nickell <john.r.nickell@gmail.com>
2026-08-19 23:15:11 -0500

    loading services

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,25 +2,25 @@
 
 declare(strict_types=1);
 
-use App\Service\StarterGreeting;
-use Fight\Common\Application\Service\Container;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
+use Psr\Container\ContainerInterface;
+use Slim\App;
 use Slim\Factory\AppFactory;
 use Slim\Psr7\Factory\ResponseFactory;
 
 require sprintf('%s/vendor/autoload.php', dirname(__DIR__));
+$container = require sprintf('%s/config/services.php', dirname(__DIR__));
 
-$container = new Container();
-$container->set(StarterGreeting::class, static fn (): StarterGreeting => new StarterGreeting());
+/**
+ * Loads application routes
+ */
+function load_routes(ContainerInterface $container, App $app): void
+{
+    include sprintf('%s/config/routes.php', dirname(__DIR__));
+}
 
 AppFactory::setContainer($container);
 AppFactory::setResponseFactory(new ResponseFactory());
 $app = AppFactory::create();
-$app->get('/', function (ServerRequestInterface $request, ResponseInterface $response) use ($container): ResponseInterface {
-    $response->getBody()->write($container->get(StarterGreeting::class)->message());
-
-    return $response;
-});
+load_routes($container, $app);
 
 return $app;
---
+++ config/parameters/paths.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Fight\Common\Application\Service\Container;
+
+/** @var Container $container */
+$rootDir = dirname(__DIR__, 2);
+$container['app.project_dir'] = $rootDir;
+$container['app.bin_dir'] = sprintf('%s/bin', $rootDir);
+$container['app.bootstrap_dir'] = sprintf('%s/bootstrap', $rootDir);
+$container['app.config_dir'] = sprintf('%s/config', $rootDir);
+$container['app.etc_dir'] = sprintf('%s/etc', $rootDir);
+$container['app.planning_dir'] = sprintf('%s/planning', $rootDir);
+$container['app.public_dir'] = sprintf('%s/public', $rootDir);
+$container['app.scripts_dir'] = sprintf('%s/scripts', $rootDir);
+$container['app.src_dir'] = sprintf('%s/src', $rootDir);
+$container['app.tests_dir'] = sprintf('%s/tests', $rootDir);
+$container['app.vendor_dir'] = sprintf('%s/vendor', $rootDir);
